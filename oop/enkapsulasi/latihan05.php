13ad38114c0ef65ee13e182a24a11e6bb542d4d9
liraRahmawati17 <lira_180@smkassalaambandung.sch.id>
2021-09-17 20:44:59 -0700

    Push

--- oop/enkapsulasi/latihan05.php
+++ oop/enkapsulasi/latihan05.php
@@ -4,69 +4,139 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>pembayaran</title>
+    <title>Document</title>
 </head>
 <body>
-    <form action="">
+    <form action="" method="post">
         <fieldset>
-            <legend>pembayaran</legend>
+            <legend>Pembayaran Dari  Aplikas Gofud</legend>
             <table>
                 <tr>
-                    <td>nama barang</td>
+                    <td>Nama Barang</td>
                     <td>:</td>
-                    <td><input type="text" name="barang"></td>
+                    <td><input type="text" name="namabarang"></td>
                 </tr>
                 <tr>
-                    <td>harga</td>
+                    <td>Harga</td>
                     <td>:</td>
-                    <td><input type="number" name = "harga"></td>
+                    <td><input type="number" name="harga"></td>
                 </tr>
                 <tr>
-                    <td>jumlah pesanan</td>
+                    <td>Jumlah Pesanan</td>
                     <td>:</td>
-                    <td><input type="number" name = "pesanan"></td>
+                    <td><input type="number" name="jumlahpesanan"></td>
                 </tr>
-                <tr>
-                    <td>jenis pembayaran</td>
-                    <td>:</td>
-                    <td><input type="text" name = "pembayaran"></td>
-                </tr>
-                <tr>
-                    <td>diskon</td>
-                    <td>:</td>
-                    <td><input type="number" name ="diskon"></td>
-                </tr>
-                <tr>
-                    <td>cashback</td>
-                    <td>:</td>
-                    <td><input type="number" name = "cashback"></td>
-                </tr>
-                <tr>
-                    <td>jumlah yang harus dibayar</td>
-                    <td>:</td>
-                    <td><input type="number" name ="bayar"></td>
-                </tr>
-                <tr>
-                    <td><input type="submit" name ="submit" value ="masuk">
-                    <input type="submit" name ="reset" value ="reset"></td>
-                </tr>
-            </table>
 
-            <?php
+                    <tr>
+            <td>Sistem Pembayaran </td>
+            <td> : </td>
+            <td><select type="text" name="sistem" >
+            <option value= "Cash" > Cash</option>
+            <option value= "M-Banking" > M-Banking</option>
+            <option value= "Gopay" > Gopay</option>
+            <option value= "PikUp" > PikUp</option>
+            </select></td>
+            </tr>
+            <tr>
+                 <td></td><td></td>
+                 <td><input type="submit" name="input" value="Input"></td>
+                </tr>
 
-if (isset($_POST['submit'])) {
-    $barang = $_POST['barang'];
+
+
+    </form>
+      </table>
+        </fieldset>
+</body>
+</html>
+    <?php
+if (isset($_POST['input'])) {
+    $namabarang = $_POST['namabarang'];
+    $jumlahpesanan = $_POST['jumlahpesanan'];
     $harga = $_POST['harga'];
-    $pesanan = $_POST['pesanan'];
-    $diskon = $_POST['diskon'];
-    $cashback = $_POST['cashback'];
-    $bayar = $_POST['bayar'];
+    $sistem = $_POST['sistem'];
+    $harganormal = $harga * $jumlahpesanan;
+    class pembayaran
+    {
+        public function __construct($namabarang, $jumlahpesanan, $harga,
+            $sistem, $harganormal) {
+            $this->namabarang = "$namabarang";
+            $this->jumlahpesanan = $jumlahpesanan;
+            $this->harga = $harga;
+            $this->sistem = $sistem;
+            $this->harganormal = $harganormal;
+        }
+        public function discount()
+        {
+            if ($this->harganormal >= 150000) {
+                $hargadiscount = $this->harganormal * 0.1;
+            } elseif ($this->harga >= 250000) {
+                $hargadiscount = $this->harganormal * 0.15;
+            } else {
+                $hargadiscount = 0;
+            }
+            return $hargadiscount;
+        }
+
+    }
+// turunkan class Malaikat ke chromebook
+    class systemPembayaran extends pembayaran
+    {
+        public function discountTambahan()
+        {
+            if ("Cash" == $this->sistem) {
+                $a = $this->discount();
+
+            } elseif ("M-Banking" == $this->sistem) {
+                $a = ($this->harganormal * 0.025) + $this->discount();
+
+            } elseif ("Gopay" == $this->sistem) {
+                $a = ($this->harganormal * 0.1) + $this->discount();
+
+            } elseif ("PikUp" == $this->sistem) {
+                $a = ($this->harganormal * 0.015) + $this->discount();
+
+            }
+
+            return $a;
+        }
+        public function cashback()
+        {
+            if ("Cash" == $this->sistem) {
+                $a = "Tidak mendapatkan CashBack";
+
+            } elseif ("M-Banking" == $this->sistem) {
+                $a = "getDiskon";
+
+            } elseif ("Gopay" == $this->sistem) {
+                $a = "CashBack";
+
+            } elseif ("PikUp" == $this->sistem) {
+                $a = "getDiskon";
+
+            }
+
+            return $a;
+        }
+        public function status()
+        {
+            $b = $this->harganormal - $this->discountTambahan();
+            $a = $b - $this->discount();
+            return $a;
+        }
+    }
+    $output = new systemPembayaran($namabarang, $jumlahpesanan, $harga,
+        $sistem, $harganormal);
+
+    echo "Nama Barang  :  " . $output->namabarang . "<br>";
+    echo "Harga Barang  :  " . $output->harga . "<br>";
+    echo "Jumlah Pesanan  :  " . $output->jumlahpesanan . "<br>";
+    echo "Metode Pembayaran  :  " . $output->sistem . "<br>";
+    echo "Harga Normal  :  " . $output->harganormal . "<br>";
+    echo "Discount :  " . $output->discount() . "<br>";
+    echo " " . $output->cashback() . " : " . $output->discountTambahan() . "<br>";
+    echo "Harga Total :  " . $output->status() . "<br>";
+
 }
 
 ?>
-
-
-        </fieldset>
-    </form>
-</body>
-</html>
